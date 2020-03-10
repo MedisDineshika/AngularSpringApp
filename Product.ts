14a67526f0abe83c92413c54338d557a1e87e905
WDM <52590387+MedisDineshika@users.noreply.github.com>
2020-03-10 07:11:29 +0530

    Angular Files

---
+++ Product.ts
@@ -0,0 +1,13 @@
+
+export class Product{
+Integer :id;
+product_name :String;
+price :long ;
+qty :int ;
+description :String ;
+colour :String ;
+size : String;
+brand :String ;
+category: String  ;
+availabilty :String ;
+}
